40f23ffa00eeeea6a9752b54f5546b74cb352396
Endre Larsen <endre.larsen93@gmail.com>
2015-04-19 16:31:40 +0200

    La til sorteringsfunksjon i render

--- project/core/src/com/tdt4240/RawHeroes/independent/Position.java
+++ project/core/src/com/tdt4240/RawHeroes/independent/Position.java
@@ -8,7 +8,7 @@ import java.io.Serializable;
 /**
  * Created by Endre on 16.04.2015.
  */
-public class Position implements Serializable{
+public class Position implements Serializable, Comparable<Position>{
     private int x;
     private int y;
     public Position(int x, int y){
@@ -72,5 +72,12 @@ public class Position implements Serializable{
         result = prime * result + NumberUtils.floatToIntBits(x);
         result = prime * result + NumberUtils.floatToIntBits(y);
         return result;
+    }
+
+    @Override
+    public int compareTo(Position p) {
+        if(this.getY() > p.getY()) return 1;
+        if(this.getY() < p.getY()) return -1;
+        return 0;
     }
 }
