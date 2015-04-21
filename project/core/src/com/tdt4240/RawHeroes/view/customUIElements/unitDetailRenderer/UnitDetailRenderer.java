32fcaa676a9a044d8133468abe37d8ead73c720b
wiltis <william.tisdall@gmail.com>
2015-04-21 12:59:22 +0200

    UnitDetailRenderer
    Finished

--- project/core/src/com/tdt4240/RawHeroes/view/customUIElements/unitDetailRenderer/UnitDetailRenderer.java
+++ project/core/src/com/tdt4240/RawHeroes/view/customUIElements/unitDetailRenderer/UnitDetailRenderer.java
@@ -1,11 +1,15 @@
 package com.tdt4240.RawHeroes.view.customUIElements.unitDetailRenderer;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
@@ -16,6 +20,7 @@ import com.tdt4240.RawHeroes.gameLogic.models.IBoard;
 import com.tdt4240.RawHeroes.gameLogic.models.IUnit;
 import com.tdt4240.RawHeroes.independent.GameConstants;
 import com.tdt4240.RawHeroes.independent.Position;
+import com.tdt4240.RawHeroes.independent.TextureChanger;
 import com.tdt4240.RawHeroes.view.customUIElements.unitRenderer.specificUnitRenderer.howToUse.IRenderNoPos;
 
 import javax.xml.soap.Text;
@@ -30,6 +35,7 @@ public class UnitDetailRenderer implements IRenderNoPos {
     private Skin skin;
     private boolean hide;
     private IBoard board;
+    private Sprite sprite;
 
     public UnitDetailRenderer(IBoard board){
         this.board = board;
@@ -67,38 +73,63 @@ public class UnitDetailRenderer implements IRenderNoPos {
         Label labelUnit = new Label(String.valueOf(unit.getIdentifier()), skin);
         labelUnit.setSize(GameConstants.TEXTFIELD_WIDTH, GameConstants.TEXTFIELD_HEIGHT);
         labelUnit.setPosition(createUnitDetailsScreenDialog.getWidth()/8, createUnitDetailsScreenDialog.getHeight()*6/8);
-
-        
+        Texture texture;
+        //Image
+        switch (unit.getIdentifier()) {
+            case STANDARD_UNIT:
+                texture = new Texture(Gdx.files.internal("units/soldierSheet.png"));
+                break;
+            case STANDARD_UNIT_2:
+                //TODO Fyll inn riktig .png-fil under
+                texture = new Texture(Gdx.files.internal("units/soldierSheet.png"));
+                break;
+            case UNIT2:
+                //TODO Fyll inn riktig .png-fil under
+                texture = new Texture(Gdx.files.internal("units/soldierSheet.png"));
+                break;
+            default:
+                texture = new Texture(Gdx.files.internal("units/soldierSheet.png"));
+        }
+        if (unit.isPlayer1Unit()){
+            sprite = new Sprite(unit.getActiveFrame(TextureChanger.changeColor(texture, Color.RED)));
+        } else {
+            sprite = new Sprite(unit.getActiveFrame(TextureChanger.changeColor(texture, Color.BLUE)));
+        }
+        Image img = new Image(sprite);
+        img.setSize(createUnitDetailsScreenDialog.getWidth()*3/8, createUnitDetailsScreenDialog.getHeight()/2);
+        img.setPosition(createUnitDetailsScreenDialog.getWidth()/8, createUnitDetailsScreenDialog.getHeight()*2/8);
 
         //Labels
         int col2 = (int) createUnitDetailsScreenDialog.getWidth()*1/2;
         Label labelHealth = new Label("Health:", skin);
         labelHealth.setSize(GameConstants.LABEL_WIDTH, GameConstants.LABEL_HEIGHT);
-        labelHealth.setPosition(col2, createUnitDetailsScreenDialog.getHeight()*5/6);
+        labelHealth.setPosition(col2, createUnitDetailsScreenDialog.getHeight()*4/7);
         Label labelAttack = new Label("Damage:", skin);
         labelAttack.setSize(GameConstants.LABEL_WIDTH, GameConstants.LABEL_HEIGHT);
-        labelAttack.setPosition(col2, createUnitDetailsScreenDialog.getHeight()*4/6);
+        labelAttack.setPosition(col2, createUnitDetailsScreenDialog.getHeight()*3/7);
         Label labelArmor = new Label("Armor:", skin);
         labelArmor.setSize(GameConstants.LABEL_WIDTH, GameConstants.LABEL_HEIGHT);
-        labelArmor.setPosition(col2, createUnitDetailsScreenDialog.getHeight()*3/6);
+        labelArmor.setPosition(col2, createUnitDetailsScreenDialog.getHeight()*2/7);
 
         //Values
         int col3 = (int) createUnitDetailsScreenDialog.getWidth()*4/6;
         TextField textFieldHealth = new TextField(String.valueOf(unit.getHealth()) + "/" + String.valueOf(unit.getMaxHealth()), skin);
         textFieldHealth.setSize(GameConstants.TEXTFIELD_WIDTH/2, GameConstants.TEXTFIELD_HEIGHT);
         textFieldHealth.setAlignment(Align.center);
-        textFieldHealth.setPosition(col3, createUnitDetailsScreenDialog.getHeight()*5/6);
+        textFieldHealth.setPosition(col3, createUnitDetailsScreenDialog.getHeight()*4/7);
 
         int[] attackDmg = unit.getAttackDmg();
         TextField textFieldAttack = new TextField(String.valueOf(attackDmg[0]) + "-" + String.valueOf(attackDmg[1]), skin);
         textFieldAttack.setSize(GameConstants.TEXTFIELD_WIDTH/2, GameConstants.TEXTFIELD_HEIGHT);
         textFieldAttack.setAlignment(Align.center);
-        textFieldAttack.setPosition(col3, createUnitDetailsScreenDialog.getHeight()*4/6);
+        textFieldAttack.setPosition(col3, createUnitDetailsScreenDialog.getHeight()*3/7);
 
         TextField textFieldArmor = new TextField(String.valueOf(unit.getArmor()), skin);
         textFieldArmor.setSize(GameConstants.TEXTFIELD_WIDTH/2, GameConstants.TEXTFIELD_HEIGHT);
         textFieldArmor.setAlignment(Align.center);
-        textFieldArmor.setPosition(col3, createUnitDetailsScreenDialog.getHeight()*3/6);
+        textFieldArmor.setPosition(col3, createUnitDetailsScreenDialog.getHeight()*2/7);
+
+        createUnitDetailsScreenDialog.clear();
 
         createUnitDetailsScreenDialog.addActor(labelUnit);
         createUnitDetailsScreenDialog.addActor(labelHealth);
@@ -107,5 +138,6 @@ public class UnitDetailRenderer implements IRenderNoPos {
         createUnitDetailsScreenDialog.addActor(textFieldHealth);
         createUnitDetailsScreenDialog.addActor(textFieldAttack);
         createUnitDetailsScreenDialog.addActor(textFieldArmor);
+        createUnitDetailsScreenDialog.addActor(img);
     }
 }
